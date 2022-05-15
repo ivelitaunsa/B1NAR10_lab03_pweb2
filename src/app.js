60e4cfad9fdc58aa87cfe1f709dc51f40b711fdf
Eberth Wilfredo Azurin Zuñiga <eazurin@unsa.edu.pe>
2022-05-15 15:52:53 -0500

    Creando primera parte de la llamada /crear

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.static('cliente'));
+
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
 
@@ -10,3 +12,8 @@ app.listen(3000, () => {
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+app.post('/crear', (request, response) => {
+  console.log(request.body.text);
+});
+
