3afd77f09147900ed82fbe4e1b543729f26f615c
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-15 12:38:30 -0500

    Agrengando el directorio servidor

---
+++ src/servidor/express.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+//var cors = require('cors');
+const bp = require('body-parser')
+const MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();
+const app = express()
+
+//app.use(cors());
+app.use(express.static('/'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+    extended: true
+}))
+
+app.listen(3000, () => {
+    console.log("Escuchando en: http://localhost:3000")
+})
+//Entrega el contenido del index.html al cliente
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'index.html'))
+})
+app.get('/js/script.js', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'js/script.js'))
+})
+//crea el archivo markdown que le llega y lo guarda en la carpeta archivos
+app.post('/', (request, response) => {
+    console.log(request.body)
+    let markDownText = request.body.text
+		let fileName = request.body.fileName
+    console.log(markDownText)
+		let ruta = "archivos/" + fileName + ".txt"
+    console.log(markDownText)
+		try {
+			fs.writeFileSync(ruta, markDownText)
+		} catch (err) {
+			console.error(err)
+		}
+		//prueba
+		try {
+			fs.writeFileSync("archivos/titulos.txt", title)
+		} catch (err) {
+			console.error(err)
+		}
+    let htmlText = "Funciono!"
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify({
+        text: htmlText
+    }))
+})
+
+
+
+
+
---
+++ src/servidor/js/script.js
@@ -0,0 +1,37 @@
+//Este script está destinado únicamente la interfaz de informacion y formularios (No atiende request)
+
+window.addEventListener("load", showWelcome);
+
+//Este método será reutilizado en el index.html
+function showWelcome() {
+  let html = `
+              <h2>Bienvenido</h2>
+              <p>Este es un navegador de archivos markdown con el cual puedes listar, ver y crear archivos markdown.
+               Estos archivos serán traducidos y mostrados en lenguaje html.</p>
+              <p>En este trabajo usamos las siguientes tecnologías: </p>
+              <ul>
+                <li>Html (Frontend)</li>
+                <li>Css (Frontend)</li>
+                <li>Javascript (Frontend y Backend)</li>
+                <li>Nodejs (Backend)</li>
+                <li>Módulos express y cors (Backend)</li>
+                <li>La comunicación entre cliente y sevidor se dará en formato JSON</li>
+              </ul>`;
+  document.getElementById("main").innerHTML = html;
+}
+function showNew() {
+  let html = `
+              <h2>Creando Archivo ...</h2>
+							<form action="POST" id="markupForm">
+              	Nombre del Archivo: <input type="text" id="textName" name="textName"></input> <br>
+              	Contenido: <textarea id="markupText" cols="50" rows="30"></textarea>
+								<input type="submit" value="Enviar">
+              </form>`
+
+  document.getElementById("main").innerHTML = html;
+}
+function listar() {
+  let html = `
+              <h2>Archivos creados:<h2>`
+  document.getElementById("main").innerHTML = html;
+}
