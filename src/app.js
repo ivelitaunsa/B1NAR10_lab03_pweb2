84bb774085afff7b725c88e58c4327aa7c437dfd
Eberth Wilfredo Azurin Zuñiga <eazurin@unsa.edu.pe>
2022-05-15 16:59:15 -0500

    Corrigiendo modulos

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
-const app = express()
 const bp = require('body-parser')
+const MarkdownIt = require('markdown-it'),
+  md = new MarkdownIt();
+const app = express()
+
+app.use(express.static('cliente'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('cliente'))
 
 app.listen(3000, () => {
-    console.log("Escuchando en: http://localhost:3000")
+  console.log("Escuchando en: http://localhost:3000")
 
 })
 
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'index.html'));
+  response.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
 app.post('/crear', (request, response) => {
