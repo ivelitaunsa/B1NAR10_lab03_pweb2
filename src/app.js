6a8f439c9685f69bea2ad55e9aa3fd27098cf553
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-16 17:01:59 -0500

    Se utilizara app.js en lugar de servidor/

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+var cors = require('cors');
+const bp = require('body-parser')
+const app = express()
+app.use(cors());
+app.use(express.static('cliente'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+  extended: true
+}))
+
+app.listen(3000, () => {
+  console.log("Escuchando en: http://localhost:3000")
+})
+
+app.get('/', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '/cliente/index.html'));
+});
+
+app.post('/crear', (request,response) => {
+  console.log(request.body)
+})
